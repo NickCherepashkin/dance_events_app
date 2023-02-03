80dfe0ca85fc35c8052f29696c50dfa4c07e1611
Nika Drozdova <cherepawka13@mail.ru>
2023-02-03 23:39:46 +0300

    add fragment of events

---
+++ app/src/main/java/com/drozdova/danceevents/EventsListAdapter.kt
@@ -0,0 +1,18 @@
+package com.drozdova.danceevents
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class EventsListAdapter : RecyclerView.Adapter<EventsListViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventsListViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: EventsListViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}   
---
+++ app/src/main/java/com/drozdova/danceevents/EventsListViewHolder.kt
@@ -0,0 +1,8 @@
+package com.drozdova.danceevents
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
+class EventsListViewHolder(view: View) : ViewHolder(view) {
+
+}
