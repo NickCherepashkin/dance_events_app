96a400efced8f598c85f3fac9eee5dfd854200a7
Nika Drozdova <cherepawka13@mail.ru>
2023-02-10 01:31:21 +0300

    add custom calendar

--- app/src/main/java/com/drozdova/danceevents/DateAdapter.kt
+++ app/src/main/java/com/drozdova/danceevents/DateAdapter.kt
@@ -1,4 +1,31 @@
 package com.drozdova.danceevents
 
-class DateAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class DateAdapter : RecyclerView.Adapter<DateViewHolder>() {
+
+    private var days: Int = 0
+    private var year: Int = 0
+    private var spaces: Int = 0
+
+    fun submit(days: Int, pos: Int, Y: Int, spaces: Int) {
+        this.days = days
+        year = Y
+        this.spaces = spaces
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DateViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.date_item, parent, false)
+        return DateViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: DateViewHolder, position: Int) {
+        holder.bind(days, position, year, spaces)
+    }
+
+    override fun getItemCount(): Int {
+        return days + spaces
+    }
 }
--- app/src/main/java/com/drozdova/danceevents/MonthViewHolder.kt
+++ app/src/main/java/com/drozdova/danceevents/MonthViewHolder.kt
@@ -1,4 +1,20 @@
 package com.drozdova.danceevents
 
-class MonthViewHolder {
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.google.android.material.textview.MaterialTextView
+
+class MonthViewHolder (private val view: View) : ViewHolder(view) {
+    fun bind(month: String, days: Int, pos: Int, Y: Int, spaces: Int) {
+        val rvDate = view.findViewById<RecyclerView>(R.id.rv_date)
+        val tvMonth = view.findViewById<MaterialTextView>(R.id.tv_month)
+
+        tvMonth.text = month
+        rvDate.setHasFixedSize(true)
+        rvDate.isNestedScrollingEnabled = true
+        val dateAdapter = DateAdapter()
+        rvDate.adapter = dateAdapter
+        dateAdapter.submit(days, pos, Y, spaces)
+    }
 }
