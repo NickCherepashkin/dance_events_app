f16246052661369dce62facd0651885edf62c40b
Nika Drozdova <cherepawka13@mail.ru>
2023-02-10 11:12:56 +0300

    add fragment EventInfo

--- app/src/main/java/com/drozdova/danceevents/MainActivity.kt
+++ app/src/main/java/com/drozdova/danceevents/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         supportFragmentManager.beginTransaction()
-            .add(R.id.fragmentContainerView, CalendarFragment())
+            .add(R.id.fragmentContainerView, EventInfoFragment())
             .commit()
     }
 }
